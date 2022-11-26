59683dd9140b2c9f0b74139def83b81f539e50dd
nishat <nishatroyhana011@gmail.com>
2022-11-26 10:28:06 +0600

    book saved to db

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ async function run(){
     try{
         const categoriesCollection = client.db("boibazar").collection("categories");
         const userCollection = client.db("boibazar").collection("users");
+        const bookCollection = client.db("boibazar").collection("books");
 
         //create user
         app.post('/users', async(req, res)=>{
@@ -25,6 +26,26 @@ async function run(){
             res.send(result);
         });
 
+        //create category
+        app.post('/categories', async(req, res)=>{
+            const cate = req.body;
+            const result = await categoriesCollection.insertOne(cate);
+            res.send(result);
+        });
+
+        //post a book
+        app.post('/products', async(req, res)=>{
+            const book = req.body;
+            const result = await bookCollection.insertOne(book);
+            res.send(result);
+        });
+
+        //get categories
+        app.get('/categories', async(req, res)=>{
+            const query = {};
+            const result = await categoriesCollection.find(query).toArray();
+            res.send(result)
+        });
 
         //create token
         app.get('/jwt',async (req, res)=>{
@@ -62,12 +83,15 @@ async function run(){
             res.send({isBuyer: user?.role === 'buyer'});
         })
 
+        //get user by email
         app.get('/users', async(req, res)=>{
             const email = req.query.email;
             const query = {email: email};
             const user = await userCollection.findOne(query);
             res.send(user)
         });
+
+        
 
     }finally{
 
